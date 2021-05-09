ad66f13cc2d207b2af92e765d4d6c78636c0c913
rets-mah-ekaj <retsmahekaj@gmail.com>
2021-05-09 10:17:51 +0530

    Uploaded Solutions
    As of 9 May 2021 10:17 AM

--- Kattis/speedlimit.cpp
+++ Kattis/speedlimit.cpp
@@ -1,22 +1,26 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
+  cin.tie(NULL);
+
   int n;
   while(true) {
     cin >> n;
     if (n == -1)
-      return 0;
-    int miles = 0, temp = 0;
-    for(int i=0; i<n; i++) {
-      int s, t;
-      cin >> s >> t;
-      t = t - temp;
-      miles += s * t;
-      temp = t;
+      break;
+    vector <pair <int, int> > v(n);
+    for (auto &i : v) {
+      pair <int, int> p;
+      cin >> p.first >> p.second;
+      i = p;
     }
+    int miles = v[0].first * v[0].second;
+    for (int i = 1; i < n; ++i)
+      miles += v[i].first * (v[i].second - v[i - 1].second);
     cout << miles << " miles\n";
   }
+  return 0;
 }
